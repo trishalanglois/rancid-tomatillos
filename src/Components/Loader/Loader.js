28e45f446a028a3cf3da9d7fe072755d02c6e8fb
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-12-18 08:42:53 -0700

    Add gif for loading component and remove older more complicated one

---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <>
+      <img src='../../images/film_projector.gif' alt='old timey film projector' />
+    </>
+  )
+}
