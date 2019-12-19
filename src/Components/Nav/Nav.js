41a3308ce6f33756e959e6f1f1213b04243db94d
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-12-19 13:22:53 -0700

    Update Redux state when user logs out to isLoggedIn: false

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import './Nav.scss';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loggedIn } from '../../actions/actions';
 
-const Nav = () => {
+export const Nav = ({ isLoggedIn, login }) => {
   return(
     <header>
       <img  />
       <h1>Rancid Tomatillos</h1>
-      <Link to='/login'>
-        <button type='button'>Login</button>
-      </Link>
+        {!isLoggedIn ?
+          <Link to='/login'>
+            <button type='button'>Login</button>
+          </Link>
+          :
+        <Link to='/'>
+          <button type='button' onClick={() => login(false)}>Logout</button>
+        </Link>
+      }
     </header>
   )
 }
 
-export default Nav;
+export const mapState = state => ({
+  isLoggedIn: state.loggedIn
+})
+
+export const mapDispatch = dispatch => ({
+  login: boolean => dispatch(loggedIn(boolean))
+})
+
+export default connect(mapState, mapDispatch)(Nav);
