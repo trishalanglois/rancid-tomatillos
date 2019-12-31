9ece368e44f83e8f0237dc9aeb80f2f89b0b3566
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-12-30 21:21:37 -0700

    Add rounding to average rating and hover styling to button

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -9,10 +9,14 @@ export const Movie = (props) => {
     <section className="movie-card">
       <img className='movie-poster-img' src={props.movie.poster_path}/>
       <div className="movie-card-bottom">
-        <p>{props.movie.average_rating}</p>
-        <h2>{props.movie.title}</h2>
+        <p className="average-rating">
+
+        {Math.round(props.movie.average_rating)}
+        
+        </p>
+        <h2 className="movie-title">{props.movie.title}</h2>
         <Link to={`/movies/${props.movie.id}`}>
-          <button onClick={() => props.getCurrentMovie(props.movie)}>Start the Show</button>
+          <button className="movie-button" onClick={() => props.getCurrentMovie(props.movie)}>Start the Show</button>
         </Link>
       </div>
     </section>
