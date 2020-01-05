2eedc172b5d99cc4ad3427c726309f6dc0cb5a7c
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-04 20:54:15 -0700

    Add styling to user rating

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -17,10 +17,7 @@ export const Movie = (props) => {
         <p className="average-rating">
         Avg. Rating: {Math.round(props.movie.average_rating)} / 10
         </p>
-
         {userRating &&  <p className="user-rating"> Your Rating: {userRating.rating}</p>}
-
-
         <h2 className="movie-title">{props.movie.title}</h2>
         <Link to={`/movies/${props.movie.id}`}>
           <button className="movie-button" onClick={() => props.getCurrentMovie(props.movie)}>START THE SHOW</button>
