73b83582d1c4042d8640b8e12b2bf7bd273da99e
sertmer <scottertmer@gmail.com>
2020-01-05 10:02:59 -0700

    Add ability to click anywhere on movie card to be routed to movieshow page

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -6,18 +6,18 @@ import { connect } from 'react-redux';
 
 export const Movie = (props) => {
   return(
-    <section className="movie-card">
-      <img className='movie-poster-img' src={props.movie.poster_path}/>
-      <div className="movie-card-bottom">
-        <p className="average-rating">
-        Avg. Rating: {Math.round(props.movie.average_rating)} / 10
-        </p>
-        <h2 className="movie-title">{props.movie.title}</h2>
-        <Link to={`/movies/${props.movie.id}`}>
-          <button className="movie-button" onClick={() => props.getCurrentMovie(props.movie)}>START THE SHOW</button>
-        </Link>
-      </div>
-    </section>
+    <Link to={`/movies/${props.movie.id}`}>
+      <section className="movie-card" onClick={() => props.getCurrentMovie(props.movie)}>
+        <img className='movie-poster-img' src={props.movie.poster_path}/>
+        <div className="movie-card-bottom">
+          <p className="average-rating">
+          Avg. Rating: {Math.round(props.movie.average_rating)} / 10
+          </p>
+          <h2 className="movie-title">{props.movie.title}</h2>
+            <button className="movie-button" onClick={() => props.getCurrentMovie(props.movie)}>START THE SHOW</button>
+        </div>
+      </section>
+    </Link>
   )
 }
 
