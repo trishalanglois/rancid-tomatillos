ec1eea2fa1f0c81e40abaa725910039c2c48a966
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-06 15:40:35 -0700

    Add propTypes to MoviesContainer component

--- src/Components/MoviesContainer/MoviesContainer.js
+++ src/Components/MoviesContainer/MoviesContainer.js
@@ -3,6 +3,7 @@ import './MoviesContainer.scss';
 import Movie from '../Movie/Movie';
 import Loader from '../Loader/Loader';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 export const MoviesContainer = (props) => {
   const { movies } = props;
@@ -30,3 +31,7 @@ export const mapState = state => ({
 });
 
 export default connect(mapState)(MoviesContainer);
+
+MoviesContainer.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object)
+}
