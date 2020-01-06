a7b937dc3e326ef1a016c9487caba07db4608a24
sertmer <scottertmer@gmail.com>
2020-01-06 14:48:37 -0700

    Add conditional snapshot to movieShowPage

--- src/Components/MovieShowPage/MovieShowPage.test.js
+++ src/Components/MovieShowPage/MovieShowPage.test.js
@@ -55,6 +55,52 @@ describe('MovieShowPage', () => {
         expect(wrapper.debug()).toMatchSnapshot()
       });
 
+      it('should match the snapshot with no rating match', () => {
+        mockProps = {
+          currentMovie: {
+            id: 1,
+            title: "Jumanji: The Next Level",
+            poster_path: "url.com",
+            backdrop_path: "url.com",
+            release_date: "2019-12-04",
+            overview: "description",
+            average_rating: 6
+        },
+          currentUser: {
+            id: 1,
+            name: 'Rick'
+          },
+          ratings: [
+            {
+              id: 210,
+              user_id: 8,
+              movie_id: 2,
+              rating: 7,
+              created_at: "2020-01-04T20:37:36.154Z",
+              updated_at: "2020-01-04T20:37:36.154Z"
+            },
+            {
+              id: 212,
+              user_id: 8,
+              movie_id: 3,
+              rating: 3,
+              created_at: "2020-01-04T20:37:36.154Z",
+              updated_at: "2020-01-04T20:37:36.154Z"
+            }
+          ]
+        }
+
+        wrapper = shallow(
+        <MovieShowPage 
+          currentMovie={mockProps.currentMovie}
+          currentUser={mockProps.currentUser}
+          ratings={mockProps.ratings}
+        />
+        )
+
+        expect(wrapper.debug()).toMatchSnapshot()
+      })
+
       describe('handleChange', () => {
         it('should update state when handleChange is invoked', () => {
           const mockDefaultState = {rating: null}
