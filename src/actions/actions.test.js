7b6f9361de7ff265caf63d28991a984430f0a134
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-06 11:37:40 -0700

    Add testing for GET_MOVIE in actions

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -61,5 +61,25 @@ describe('Actions', () => {
 
     expect(result).toEqual(mockExpectedAction);
   });
+  
+  it('Should have a type of GET_MOVIE', () => {
+    const movie = {
+      id:1,
+      title:"Jumanji: The Next Level",
+      poster_path:"https://image.tmdb.org/t/p/original//l4iknLOenijaB85Zyb5SxH1gGz8.jpg",
+      backdrop_path:"https://image.tmdb.org/t/p/original//zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg",
+      release_date:"2019-12-04",
+      overview:"In Jumanji: The Next Level, the gang is back but the game has changed. As they return to rescue one of their own, the players will have to brave parts unknown from arid deserts to snowy mountains, to escape the world's most dangerous game.",
+      average_rating:7.142857142857143
+  } 
+
+    const result = actions.getCurrentMovie(movie);
+    const mockExpectedAction = {
+      type: 'GET_MOVIE',
+      movie
+    } 
+
+    expect(result).toEqual(mockExpectedAction);
+  });
 
 });
