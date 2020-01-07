e7bbd2c6e06473b39ec9d0cb23f8f5a4a4887193
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-07 15:26:51 -0700

    Add testing for mapState in Nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { loggedIn, getRatings } from '../../actions/actions';
 import PropTypes from 'prop-types';
 
+
 export const Nav = ({ isLoggedIn, login, clearRatings }) => {
   const logOut = () => {
     login(false);
--- src/Components/Nav/Nav.test.js
+++ src/Components/Nav/Nav.test.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import { Nav } from './Nav';
 import { shallow } from 'enzyme';
+import { logOut, mapState } from '../Nav/Nav';
 
 describe('Nav', () => {
-  it('should match the snapshot', () => {
-    let wrapper = shallow(<Nav />)
-    expect(wrapper.debug()).toMatchSnapshot()
-  })
+
+  let wrapper
+
+  describe('Nav Component', () => {
+
+    beforeEach(() => {
+      wrapper = shallow(<Nav
+        logOut={logOut}
+        isLoggedIn={true}
+        login={true}
+        clearRatings={jest.fn()}
+      />)
+    });
+
+    it('should match the snapshot', () => {
+      expect(wrapper.debug()).toMatchSnapshot()
+    })
+  });
+
+  describe('mapState', () => {
+
+    it('should return an object with isLoggedIn', () => {
+      const mockState = {
+        loggedIn: true,
+      };
+      const expected = { isLoggedIn: true };
+
+      const mappedProps = mapState(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+
+  });
+
+  describe('mapDispatch', () => {
+
+    it('', () => {
+
+    });
+
+  });
+
+
+
 })
